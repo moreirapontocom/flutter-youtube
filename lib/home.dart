46fa8e83d82f529f173a70e71baba31e71c27bed
Lucas Moreira <moreirapontocom@gmail.com>
2019-09-30 18:39:42 -0300

    AppBar buttons (actions)

---
+++ lib/home.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(color: Colors.grey),
+        title: Image.asset("images/youtube.png",
+          width: 98,
+          height: 22,
+        ),
+        // actionsIconTheme: ,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.videocam),
+            onPressed: () {
+              print("Videocam");
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              print("Search");
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.account_circle),
+            onPressed: () {
+              print("Account");
+            },
+          )
+        ],
+      ),
+      body: Container(
+        color: Colors.white,
+      ),
+    );
+  }
+}
